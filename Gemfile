95e5108ce9248cc8aaf642e37e4f78e02b584452
Vadim Eremeev <vadim@ensogo.com>
2016-08-13 11:50:25 +0300

    added simple token auth

--- Gemfile
+++ Gemfile
@@ -52,5 +52,6 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'devise'
+gem 'simple_token_authentication'
 
 gem 'rabl'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160812055530) do
+ActiveRecord::Schema.define(version: 20160813074349) do
 
   create_table "timezones", force: :cascade do |t|
     t.string   "name"
@@ -21,19 +21,21 @@ ActiveRecord::Schema.define(version: 20160812055530) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.boolean  "is_admin",               default: false
-    t.datetime "created_at",                             null: false
-    t.datetime "updated_at",                             null: false
-    t.string   "email",                  default: "",    null: false
-    t.string   "encrypted_password",     default: "",    null: false
+    t.boolean  "is_admin",                          default: false
+    t.datetime "created_at",                                        null: false
+    t.datetime "updated_at",                                        null: false
+    t.string   "email",                             default: "",    null: false
+    t.string   "encrypted_password",                default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,     null: false
+    t.integer  "sign_in_count",                     default: 0,     null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.string   "authentication_token",   limit: 30
+    t.index ["authentication_token"], name: "index_users_on_authentication_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
