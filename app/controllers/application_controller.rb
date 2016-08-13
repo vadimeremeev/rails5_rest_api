3e837e01985a244ab15155282af6ed277a6aa7fd
Vadim Eremeev <vadim@ensogo.com>
2016-08-13 11:50:57 +0300

    disable forgery for json requests

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+  protect_from_forgery with: :null_session, if: Proc.new { |c| c.request.format =~ %r{application/json} }
 
   def index
     if user_signed_in?
